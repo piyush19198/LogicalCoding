b1275464eefb9ff200948abefda22bcbbe50a709
Piyush <touchwithpiyush@gmail.com>
2022-04-13 12:16:51 +0530

    Update IterateHashMapUsingWhileAndFor.java

--- IterateHashMapUsingWhileAndFor.java
+++ IterateHashMapUsingWhileAndFor.java
@@ -1,6 +1,28 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class IterateHashMapUsingWhileAndFor {
     
     public static void main(String[] args) {
-    
+        Map<String, Integer> hashMap = new HashMap<String, Integer>();
+        hashMap.put("Piyush", 1);
+        hashMap.put("Pratik", 2);
+        hashMap.put("Tejal", 3);
+        hashMap.put("Rushikesh", 4);
+
+        for (Map.Entry<String, Integer> set : hashMap.entrySet()) {
+            System.out.println(set.getKey() + " = " + set.getValue());
+        }
+
+        hashMap.forEach((key, value) -> System.out.println(key + " = " + value));
+        
+        Iterator<Entry<String, Integer>> itr = hashMap.entrySet().iterator();
+        while (itr.hasNext()) {
+            Map.Entry<String,Integer> itrHM = (Map.Entry<String,Integer>)
+            itr.next();
+            System.out.println(itrHM.getKey() +" = " + itrHM.getValue());
+        }
     }
 }
