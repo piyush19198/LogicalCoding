1ce437f279162528fc17229a889c8abe38aac771
Piyush <touchwithpiyush@gmail.com>
2022-04-24 16:25:09 +0530

    Create AdRotation.java

---
+++ AdRotation.java
@@ -0,0 +1,19 @@
+public class AdRotation
+{
+    static void invertBits(int num)
+    {
+        int x = (int)(Math.log(num) /
+                      Math.log(2)) + 1;
+     
+        for (int i = 0; i < x; i++)
+        num = (num ^ (1 << i));
+     
+        System.out.print(num);
+    }
+
+    public static void main(String[] args)
+    {
+        int num = 50;
+        invertBits(num);
+    }
+}
