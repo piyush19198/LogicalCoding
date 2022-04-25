b2495e3da278eb01aed366559da3e4e8812c58c3
Piyush <touchwithpiyush@gmail.com>
2022-04-25 17:49:56 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,12 +1,13 @@
 public class BinarySearch {
     public static void binarySearch(int arr[], int low , int high, int key){
         int mid = (low + high) / 2;
+
         while (low <= high) {
             if (arr[mid] < key) {
+                low = mid + 1;
+            } else if (arr[mid] == key) {
                 System.out.println("Element is found at index : " + mid);
                 break;
-            } else if (arr[mid] == key) {
-                low = mid + 1;
             } else {
                 high = mid - 1;
             }
@@ -21,7 +22,7 @@ public class BinarySearch {
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         int n = arr.length;
         int high = n - 1;
-        int key = 7;
+        int key = 10;
         binarySearch(arr, 0, high, key);
     }
 }
