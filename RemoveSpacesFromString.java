6f52f9a50c6d1e4a249362207200ca7e4b09c1e5
Piyush <touchwithpiyush@gmail.com>
2022-04-30 12:04:18 +0530

    Update RemoveSpacesFromString.java

--- RemoveSpacesFromString.java
+++ RemoveSpacesFromString.java
@@ -2,12 +2,15 @@
 public class RemoveSpacesFromString {
     public static void main(String[] args) {
         String str = "Piyush Ramdas Warke";
-        
+        String newstr = "";
         char[] arr = str.toCharArray();
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != ' ') {
+
                 System.out.print(arr[i]);
             }
         }
+        // newstr = String.valueOf(arr);
+        // System.out.println(newstr);
     }
 }
